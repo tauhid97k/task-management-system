6094ae4590f9ee7f64ffcaef5cdd51bf098ae48a
tauhid97k <tauhid.coder@gmail.com>
2024-09-21 19:51:46 +0600

    laravel-backend: added auth check, registration, login, logout functionalities, and more.
    - Add Login and Registration Validations
    - Add Auth check, Login, Register, and Logout functionalities
    - Add UserResource to modify the api response

--- laravel-backend/app/Http/Controllers/Api/Auth/AuthController.php
+++ laravel-backend/app/Http/Controllers/Api/Auth/AuthController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\LoginRequest;
+use App\Http\Requests\Api\Auth\RegisterRequest;
+use App\Http\Resources\Api\User\UserResource;
+use App\Models\User;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -13,24 +19,43 @@ class AuthController extends Controller
     // Auth Check
     public function auth()
     {
-        //
+        return response()->json([
+            'isAuthenticated' => auth('web')->check(),
+            'user' => new UserResource(auth('web')->user())
+        ]);
     }
 
     // Register User
-    public function register()
+    public function register(RegisterRequest $request)
     {
-        // 
+        User::create($request->validated());
+
+        return $this->success('Registration Successful');
     }
 
     // Login User
-    public function login()
+    public function login(LoginRequest $request)
     {
-        //
+        if (Auth::attempt($request->only('email', 'password'), $request->remember)) {
+
+            $request->session()->regenerate();
+
+            return $this->success('Login Successful');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => trans('auth.failed'),
+        ]);
     }
 
     // Logout User
     public function logout(Request $request)
     {
-        //
+        Auth::guard("web")->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return $this->success('You are now logged out');
     }
 }
---
+++ laravel-backend/app/Http/Requests/Api/Auth/RegisterRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Api\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'string', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'status' => [
+                'required',
+                'string',
+                Rule::in(['pending', 'inprogress', 'completed']),
+            ]
+        ];
+    }
+}
